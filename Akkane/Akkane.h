f4bd89fce1cf1be31da665b04f1780a5bcb5c4e6
pjechris <jcpastant@xebia.fr>
2014-12-30 16:21:00 +0100

    project setup

---
+++ Akkane/Akkane.h
@@ -0,0 +1,19 @@
+//
+// This file is part of Akkane
+//
+// Created by JC on 30/12/14.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Akkane.
+FOUNDATION_EXPORT double AkkaneVersionNumber;
+
+//! Project version string for Akkane.
+FOUNDATION_EXPORT const unsigned char AkkaneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Akkane/PublicHeader.h>
+
+
