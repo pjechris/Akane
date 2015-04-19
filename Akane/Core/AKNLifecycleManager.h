12bf13609ce15fc52901a2c85eb56fcb5f0b437f
pjechris <jcpastant@xebia.fr>
2015-04-19 15:18:52 +0200

    [core] lifecycle manager

---
+++ Akane/Core/AKNLifecycleManager.h
@@ -0,0 +1,27 @@
+//
+// This file is part of Akane
+//
+// Created by JC on 18/04/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol AKNViewModel;
+@protocol AKNViewConfigurable;
+@protocol AKNPresenter;
+@class AKNState;
+
+@interface AKNLifecycleManager : NSObject
+
+- (instancetype)initWithPresenter:(id<AKNPresenter>)presenter;
+
+- (void)updateWithState:(AKNState *)state;
+
+- (void)mountState;
+
+- (void)updateView:(UIView<AKNViewConfigurable> *)view withViewModel:(id<AKNViewModel>)viewModel;
+
+@end
---
+++ Akane/Core/AKNLifecycleManager.m
@@ -0,0 +1,66 @@
+//
+// This file is part of Akane
+//
+// Created by JC on 18/04/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+#import "AKNLifecycleManager.h"
+#import "AKNViewConfigurable.h"
+#import "AKNViewModel.h"
+#import "AKNState.h"
+#import "AKNPresenter.h"
+#import "AKNPresenterViewController.h"
+
+@interface AKNLifecycleManager ()
+@property(nonatomic, weak)id<AKNPresenter>    presenter;
+@property(nonatomic, strong)AKNState            *state;
+@end
+
+@implementation AKNLifecycleManager
+
+- (instancetype)initWithPresenter:(id<AKNPresenter>)presenter {
+    if (!(self = [super init])) {
+        return nil;
+    }
+
+    self.presenter = presenter;
+
+    return self;
+}
+
+- (void)updateWithState:(AKNState *)state {
+    self.state = state;
+    [[self view] configure];
+}
+
+- (void)mountState {
+}
+
+- (UIView<AKNViewConfigurable> *)view {
+    return self.presenter.view;
+}
+
+- (void)updateView:(UIView<AKNViewConfigurable> *)view withViewModel:(id<AKNViewModel>)viewModel {
+    NSAssert([view isDescendantOfView:[self view]], @"Attempting to call updateView: with a view which is not a subview of current one (%@)", [self view]);
+
+    if (!view.lifecycleManager) {
+    }
+    else {
+        [view.lifecycleManager.presenter setupWithViewModel:viewModel];
+    }
+}
+
+
+- (id<AKNPresenter>)createPresenterForView:(UIView<AKNViewConfigurable> *)view {
+    Class presenterClass = NSClassFromString([NSStringFromClass([view class]) stringByAppendingString:@"Controller"]);
+
+    if (presenterClass && [presenterClass conformsToProtocol:@protocol(AKNPresenter)]) {
+        return [[presenterClass alloc] initWithView:view];
+    }
+
+    return [[AKNPresenterViewController alloc] initWithView:view];
+}
+
+@end
