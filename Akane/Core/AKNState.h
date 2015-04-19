4a57daebacbb6e6a370f4b3409020ce9d21341aa
pjechris <jcpastant@xebia.fr>
2015-04-19 15:18:07 +0200

    [core] state object

---
+++ Akane/Core/AKNState.h
@@ -0,0 +1,21 @@
+//
+// This file is part of Akane
+//
+// Created by JC on 18/04/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol AKNViewConfigurable;
+@protocol AKNViewModel;
+@protocol AKNPresenter;
+@class AKNLifecycleManager;
+
+@interface AKNState : NSObject
+
+@property(nonatomic, weak, readonly)id<AKNViewModel>    viewModel;
+@property(nonatomic, strong, readonly)id                context;
+
+@end
---
+++ Akane/Core/AKNState.m
@@ -0,0 +1,13 @@
+//
+// This file is part of Akane
+//
+// Created by JC on 18/04/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+#import "AKNState.h"
+
+@implementation AKNState
+
+@end
