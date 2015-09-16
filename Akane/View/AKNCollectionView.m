1c6e737498b1b896286761eb09bba3275d9e1b3a
Pascal Drouilly <pdrouilly@xebia.fr>
2015-09-16 17:35:01 +0200

    [collection] fixed 'pod repo push'

--- Akane/View/AKNCollectionView.m
+++ Akane/View/AKNCollectionView.m
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "AKNCollectionView.h"
 #import "AKNViewModel.h"
-#import <EventListener/EventListener.h>
+#import "EventListener.h"
 
 
 @implementation AKNCollectionView
