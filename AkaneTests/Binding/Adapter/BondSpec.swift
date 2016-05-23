f253371ade35da5f6b29013f2cca05becb64cb3e
pjechris <jcpastant@xebia.fr>
2016-05-23 18:07:30 +0200

    [tests] added tests on AnyObservation

---
+++ AkaneTests/Binding/Adapter/BondSpec.swift
@@ -0,0 +1,35 @@
+//
+// This file is part of Akane
+//
+// Created by JC on 23/05/16.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+import Foundation
+import Bond
+import Nimble
+import Quick
+@testable import Akane
+
+class BondSpec : QuickSpec {
+    override func spec() {
+        var observation: AnyObservation<String?>!
+        var binding: UILabel!
+
+        beforeEach {
+            binding = UILabel()
+            observation = AnyObservation(value: "Hello Bond")
+        }
+
+        describe("bindTo") {
+            context("on bnd attribute") {
+                it("updates binding") {
+                    observation.bindTo(binding.bnd_text)
+
+                    expect(binding.text) == "Hello Bond"
+                }
+            }
+        }
+    }
+}
