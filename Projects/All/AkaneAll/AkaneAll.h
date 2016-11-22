847d9bea9ce1002792331895fee81e3e5b961d19
Simone Civetta <viteinfinite@gmail.com>
2016-11-22 18:46:14 +0100

    [core] Add AkaneAll project for testing

---
+++ Projects/All/AkaneAll/AkaneAll.h
@@ -0,0 +1,19 @@
+//
+//  AkaneAll.h
+//  AkaneAll
+//
+//  Created by Simone Civetta on 22/11/16.
+//  Copyright © 2016 Akane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AkaneAll.
+FOUNDATION_EXPORT double AkaneAllVersionNumber;
+
+//! Project version string for AkaneAll.
+FOUNDATION_EXPORT const unsigned char AkaneAllVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AkaneAll/PublicHeader.h>
+
+
