096794def647c6b6a2ddb485d468ae6d652c1acc
Simone Civetta <viteinfinite@gmail.com>
2016-11-22 20:26:16 +0100

    [project] Add AkaneCore project with specs

---
+++ Projects/Core/AkaneCore/AkaneCore.h
@@ -0,0 +1,19 @@
+//
+//  AkaneCore.h
+//  AkaneCore
+//
+//  Created by Simone Civetta on 22/11/16.
+//  Copyright © 2016 Akane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AkaneCore.
+FOUNDATION_EXPORT double AkaneCoreVersionNumber;
+
+//! Project version string for AkaneCore.
+FOUNDATION_EXPORT const unsigned char AkaneCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AkaneCore/PublicHeader.h>
+
+
