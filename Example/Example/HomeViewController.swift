040f6e5492c65b9e2d932502165f440f7938bbd7
pjechris <jcpastant@xebia.fr>
2017-12-24 19:09:17 +0100

    [example] added navbar example

--- Example/Example/HomeViewController.swift
+++ Example/Example/HomeViewController.swift
@@ -12,9 +12,16 @@ import Akane
 class HomeViewController: UIViewController, ComponentController {
     typealias ViewType = SearchAuthorsView
 
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      self.viewModel = SearchAuthorsViewModel()
-   }
+    lazy private(set) var navbarItemDisplayer: NavbarItemDisplayer = NavbarItemDisplayer(content: self.navigationItem)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.viewModel = SearchAuthorsViewModel()
+    }
+
+    func didLoadComponent() {
+        self.navbarItemDisplayer.bindings(self.observer, props: "Search")
+    }
 }
+
 
